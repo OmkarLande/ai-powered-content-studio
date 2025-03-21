f64e7659dbf77466c29cfe17bb0ab8ff72fc3d6b
sawanttejas2204 <tejassawant2563@gmail.com>
2025-03-21 18:01:14 +0530

    genscript page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import GenScript from './pages/GenScript';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/gen-script" element={<GenScript/>} />
       </Routes>
     </Router>
   );
